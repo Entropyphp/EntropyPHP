90c037733a41ba776733d74fc1db234c4cf9b61e
willy <william.lety@gmail.com>
2025-07-31 23:27:59 +0200

    First commit

--- src/Invoker/InvokerFactory.php
+++ src/Invoker/InvokerFactory.php
@@ -5,20 +5,14 @@ declare(strict_types=1);
 namespace Entropy\Invoker;
 
 use DI\Definition\Resolver\ResolverDispatcher;
-use DI\Invoker\DefinitionParameterResolver;
 use DI\Proxy\ProxyFactory;
-use Entropy\Invoker\ParameterResolver\AssociativeArrayTypeHintResolver;
 use Invoker\Invoker;
 use Invoker\InvokerInterface;
-use Entropy\Utils\File\FileUtils;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
 use Invoker\ParameterResolver\ResolverChain;
-use Invoker\ParameterResolver\DefaultValueResolver;
-use Invoker\ParameterResolver\NumericArrayResolver;
-use Invoker\ParameterResolver\Container\TypeHintContainerResolver;
 
-class InvokerFactory
+class InvokerFactory extends AbstractResolverChainFactory
 {
     /**
      * Create Invoker
@@ -42,58 +36,5 @@ class InvokerFactory
             new ResolverChain(array_merge($otherResolvers, $defaultResolvers)),
             $container
         );
-    }
-
-    /**
-     * @param ContainerInterface $container
-     * @return string|null
-     * @throws ContainerExceptionInterface
-     */
-    private function getProxyDirectory(ContainerInterface $container): ?string
-    {
-        if (!$container->has('env')) {
-            return null;
-        }
-
-        $proxyDir = null;
-
-        if ($container->get('env') === 'prod') {
-            $projectDir = FileUtils::getProjectDir();
-            $projectDir = realpath($projectDir) ?: $projectDir;
-            $proxyDir = $container->has('proxy_dir') ? $container->get('proxy_dir') : null;
-            $proxyDir = $proxyDir ? $projectDir . $proxyDir : null;
-        }
-
-        return $proxyDir;
-    }
-
-    /**
-     * @param ContainerInterface $container
-     * @param ResolverDispatcher $definitionResolver
-     * @return array
-     */
-    private function getDefaultResolvers(ContainerInterface $container, ResolverDispatcher $definitionResolver): array
-    {
-        return [
-            new DefinitionParameterResolver($definitionResolver),
-            new NumericArrayResolver(),
-            new AssociativeArrayTypeHintResolver(),
-            new DefaultValueResolver(),
-            new TypeHintContainerResolver($container),
-        ];
-    }
-
-    /**
-     * @throws ContainerExceptionInterface
-     */
-    private function getCustomResolvers(ContainerInterface $container): array
-    {
-        if (!$container->has('params.resolvers')) {
-            return [];
-        }
-
-        $resolvers = $container->get('params.resolvers');
-
-        return is_array($resolvers) ? $resolvers : [$resolvers];
     }
 }
--- src/Invoker/ResolverChainFactory.php
+++ src/Invoker/ResolverChainFactory.php
@@ -6,17 +6,11 @@ namespace Entropy\Invoker;
 
 use DI\Definition\Resolver\ResolverDispatcher;
 use DI\Proxy\ProxyFactory;
-use Entropy\Invoker\ParameterResolver\AssociativeArrayTypeHintResolver;
-use Entropy\Utils\File\FileUtils;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
-use DI\Invoker\DefinitionParameterResolver;
 use Invoker\ParameterResolver\ParameterResolver;
-use Invoker\ParameterResolver\DefaultValueResolver;
-use Invoker\ParameterResolver\NumericArrayResolver;
-use Invoker\ParameterResolver\Container\TypeHintContainerResolver;
 
-class ResolverChainFactory
+class ResolverChainFactory extends AbstractResolverChainFactory
 {
     /**
      * @param ContainerInterface $container
@@ -35,58 +29,5 @@ class ResolverChainFactory
         $otherResolvers = $this->getCustomResolvers($container);
 
         return new ControllerParamsResolver(array_merge($otherResolvers, $defaultResolvers));
-    }
-
-    /**
-     * @param ContainerInterface $container
-     * @return string|null
-     * @throws ContainerExceptionInterface
-     */
-    private function getProxyDirectory(ContainerInterface $container): ?string
-    {
-        if (!$container->has('env')) {
-            return null;
-        }
-
-        $proxyDir = null;
-
-        if ($container->get('env') === 'prod') {
-            $projectDir = FileUtils::getProjectDir();
-            $projectDir = realpath($projectDir) ?: $projectDir;
-            $proxyDir = $container->has('proxy_dir') ? $container->get('proxy_dir') : null;
-            $proxyDir = $proxyDir ? $projectDir . $proxyDir : null;
-        }
-
-        return $proxyDir;
-    }
-
-    /**
-     * @param ContainerInterface $container
-     * @param ResolverDispatcher $definitionResolver
-     * @return array
-     */
-    private function getDefaultResolvers(ContainerInterface $container, ResolverDispatcher $definitionResolver): array
-    {
-        return [
-            new DefinitionParameterResolver($definitionResolver),
-            new NumericArrayResolver(),
-            new AssociativeArrayTypeHintResolver(),
-            new DefaultValueResolver(),
-            new TypeHintContainerResolver($container),
-        ];
-    }
-
-    /**
-     * @throws ContainerExceptionInterface
-     */
-    private function getCustomResolvers(ContainerInterface $container): array
-    {
-        if (!$container->has('params.resolvers')) {
-            return [];
-        }
-
-        $resolvers = $container->get('params.resolvers');
-
-        return is_array($resolvers) ? $resolvers : [$resolvers];
     }
 }
