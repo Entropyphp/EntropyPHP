515cb7752e18e26601aa01fb631d4a81fad78954
willy <william.lety@gmail.com>
2025-08-24 18:46:24 +0200

    First commit

--- tests/AbstractApplicationTest.php
+++ tests/AbstractApplicationTest.php
@@ -265,9 +265,6 @@ class AbstractApplicationTest extends TestCase
         $this->assertSame($this->app, $container->get(ApplicationInterface::class));
         $this->assertIsString($container->get('app.project.dir'));
         $this->assertIsString($container->get('app.cache.dir'));
-        $this->assertInstanceof(KernelEvent::class, $container->get(KernelEvent::class));
-        $this->assertInstanceof(KernelMiddleware::class, $container->get(KernelMiddleware::class));
-        $this->assertInstanceof(EventDispatcherInterface::class, $container->get(EventDispatcherInterface::class));
         $this->assertSame($module::class, $container->get('module.test'));
 
         $this->removeDirectory($this->app->getProjectDir() . AbstractApplication::COMPILED_CONTAINER_DIRECTORY);
